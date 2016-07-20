538e413669a91f9c40ac75df9757a7d657e376ec
David <phsdavhuynh@gmail.com>
2016-07-20 12:43:38 -0400

    Adding handController input handler and object pickup/tossing

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class HandController : MonoBehaviour
+{
+
+    SteamVR_TrackedObject trackedObj;
+    SteamVR_Controller.Device device;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void FixedUpdate()
+    {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        // TRIGGER TOUCH HANDLERS
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+ 
+        }
+
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+         
+        }
+
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+        
+        }
+
+        // TRIGGER PRESS HANDLERS
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+   
+        }
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+   
+        }
+
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+  
+        }
+
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+
+        // attach grabbed object to controller
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+
+            col.attachedRigidbody.isKinematic = true;
+            col.gameObject.transform.SetParent(this.gameObject.transform);
+        }
+
+        // release grabbed object
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            col.gameObject.transform.SetParent(null);
+            col.attachedRigidbody.isKinematic = false;
+
+            tossObject(col.attachedRigidbody);
+        }
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+        
+        }
+
+    }
+
+    private void tossObject(Rigidbody rigidBody)
+    {
+        Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+        if (origin != null)
+        {
+            rigidBody.velocity = origin.TransformVector(device.velocity);
+            rigidBody.angularVelocity = origin.TransformVector(device.angularVelocity);
+        }
+        else
+        {
+            rigidBody.velocity = device.velocity;
+            rigidBody.angularVelocity = device.angularVelocity;
+
+        }
+    }
+}
