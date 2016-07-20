1e3164a36fb955d19be01eedec6257fc10eda55c
David <phsdavhuynh@gmail.com>
2016-07-20 13:43:23 -0400

    Add rudimentary product box initializer

---
+++ Assets/Scripts/ProductInitializer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProductInitializer : MonoBehaviour
+{
+    public string upc;
+
+    IEnumerator Start()
+    {
+        WWW productImageFront = new WWW("https://www.kroger.com/product/images/large/front/" + upc);
+        yield return productImageFront;
+
+        WWW productImageRight = new WWW("https://www.kroger.com/product/images/large/right/" + upc);
+        yield return productImageRight;
+
+        Renderer renderer = GetComponent<Renderer>();
+        Vector2 productImageFrontDimensions = new Vector2(productImageFront.texture.width, productImageFront.texture.height).normalized;
+        Vector2 productImageRightDimensions = new Vector2(productImageRight.texture.width, productImageRight.texture.height).normalized;
+
+        renderer.material.mainTexture = productImageFront.texture;
+        transform.localScale = new Vector3(productImageFrontDimensions.x * 0.25f, productImageFrontDimensions.y * 0.25f, productImageRightDimensions.x * 0.25f);
+    }
+
+}
