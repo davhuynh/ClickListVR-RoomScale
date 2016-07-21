d55660fbe720c1092039b4c2a61130c45dd5dd22
David <phsdavhuynh@gmail.com>
2016-07-21 03:31:36 -0400

    Adding SteamVR Unity Toolkit scripts, enabling teleportation

---
+++ Assets/Scripts/CartController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CartController : MonoBehaviour {
+
+    void OnTriggerEnter(Collider product)
+    {
+        Destroy(product.gameObject);
+    }
+}
