c8e875c6dfb57c1a7cdc1239b9885250822fe26f
David <phsdavhuynh@gmail.com>
2016-07-26 04:12:18 -0400

    product name displayed on cart add, controller menus

--- Assets/Scripts/CartController.cs
+++ Assets/Scripts/CartController.cs
@@ -4,9 +4,21 @@ using System.Collections;
 public class CartController : MonoBehaviour {
 
     public ParticleSystem cartSparkle;
+    public TextMesh cartAddText;
 
     void OnTriggerEnter(Collider product)
     {
         cartSparkle.Play();
+        StartCoroutine(DisplayProductAdded());
+
+        cartAddText.text = product.gameObject.GetComponent<ProductInitializer>().productName;
     }
+
+    IEnumerator DisplayProductAdded()
+    {
+        cartAddText.gameObject.SetActive(true);            
+        yield return new WaitForSeconds(5);
+        cartAddText.gameObject.SetActive(false);            
+    }
+
 }
--- Assets/Scripts/ProductInitializer.cs
+++ Assets/Scripts/ProductInitializer.cs
@@ -4,21 +4,25 @@ using System.Collections;
 public class ProductInitializer : MonoBehaviour
 {
     public string upc;
+    public string productName;
+    public bool requiresImage;
 
     IEnumerator Start()
     {
-        WWW productImageFront = new WWW("https://www.kroger.com/product/images/large/front/" + upc);
-        yield return productImageFront;
+        if(requiresImage)
+        {
+            WWW productImageFront = new WWW("https://www.kroger.com/product/images/large/front/" + upc);
+            yield return productImageFront;
 
-        WWW productImageRight = new WWW("https://www.kroger.com/product/images/large/right/" + upc);
-        yield return productImageRight;
+            WWW productImageRight = new WWW("https://www.kroger.com/product/images/large/right/" + upc);
+            yield return productImageRight;
 
-        Renderer renderer = GetComponent<Renderer>();
-        Vector2 productImageFrontDimensions = new Vector2(productImageFront.texture.width, productImageFront.texture.height).normalized;
-        Vector2 productImageRightDimensions = new Vector2(productImageRight.texture.width, productImageRight.texture.height).normalized;
+            Renderer renderer = GetComponent<Renderer>();
+            Vector2 productImageFrontDimensions = new Vector2(productImageFront.texture.width, productImageFront.texture.height).normalized;
+            Vector2 productImageRightDimensions = new Vector2(productImageRight.texture.width, productImageRight.texture.height).normalized;
 
-        renderer.material.mainTexture = productImageFront.texture;
-        transform.localScale = new Vector3(productImageFrontDimensions.x * 0.25f, productImageFrontDimensions.y * 0.25f, productImageRightDimensions.x * 0.25f);
+            renderer.material.mainTexture = productImageFront.texture;
+            transform.localScale = new Vector3(productImageFrontDimensions.x * 0.25f, productImageFrontDimensions.y * 0.25f, productImageRightDimensions.x * 0.25f);
+        }
     }
-
 }
