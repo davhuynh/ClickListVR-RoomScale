8b6756fc4d6065895ab2ec1a9202a35dab52e3e6
David <phsdavhuynh@gmail.com>
2016-07-26 03:21:20 -0400

    adding fruit picking demo

---
+++ Assets/PickedObjectController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickedObjectController : MonoBehaviour {
+
+    private Rigidbody rigidBody = null;
+
+    private void Start()
+    {
+        this.rigidBody = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider hand)
+    {
+        this.rigidBody.useGravity = true;
+    }
+}
