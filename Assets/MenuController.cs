c84fb65b0bc54bdd5b04d0d296273c40554a76da
David <phsdavhuynh@gmail.com>
2016-07-26 05:54:34 -0400

    cleaning up controller menu

---
+++ Assets/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject menuObject;
+
+    private void Start()
+    {
+        menuObject.SetActive(false);
+        GetComponent<VRTK_ControllerEvents>().AliasMenuOn += new ControllerInteractionEventHandler(DoMenuOn);
+        GetComponent<VRTK_ControllerEvents>().AliasMenuOff += new ControllerInteractionEventHandler(DoMenuOff);
+    }
+
+    private void DoMenuOn(object sender, ControllerInteractionEventArgs e)
+    {
+        menuObject.SetActive(true);
+    }
+
+    private void DoMenuOff(object sender, ControllerInteractionEventArgs e)
+    {
+        menuObject.SetActive(false);
+    }
+
+}
